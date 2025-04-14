01b9abbf70bcca6ee38530571470fd96a801ca63
AustinBarikdar <austinbarikdar@gmail.com>
2025-04-14 14:43:20 -0400

    Added a Delete function, and update completion task

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -8,6 +8,7 @@ const Dashboard: React.FC = () => {
     const [date, setDate] = React.useState("");
     const [title, setTitle] = React.useState("");
     const [description, setDescription] = React.useState("");
+    const [checkbox, setCheckbox] = React.useState(false);
 
     const context = useContext(AppContent);
 
@@ -60,6 +61,36 @@ const Dashboard: React.FC = () => {
             toast.error(data.message, toastdata);
         }
     }
+    const deleteTask = async (taskId: string, index : number) => {
+        const { data } = await axios.delete(backendURL + "/api/task/delete-task/" + taskId, {
+            withCredentials: true
+        });
+
+        if (data.success) {
+            toast.success("Successfully deleted task", toastdata);
+            const updatedList = doList.filter((task: any, i: number) => i !== index);
+            setdoList(updatedList);
+            localStorage.setItem('doList', JSON.stringify(updatedList));
+        } else {
+            toast.error(data.message, toastdata);
+        }
+    }
+
+    const completedTask = async (index: number ,taskId: string, Value: boolean) => {
+        const { data } = await axios.put(backendURL + "/api/task/update-task/" + taskId, {
+            completed: Value
+        }, { withCredentials: true });
+
+        if (data.success) {
+            toast.success("Successfully updated task", toastdata);
+            const updatedList = [...doList];
+            updatedList[index].completed = Value;
+            setdoList(updatedList);
+            localStorage.setItem('doList', JSON.stringify(updatedList));
+        } else {
+            toast.error(data.message, toastdata);
+        }
+    }
 
     return (
         <div className='min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white'>
@@ -96,19 +127,23 @@ const Dashboard: React.FC = () => {
                     <h2 className='text-2xl font-semibold text-center'>Your Tasks</h2>
                     <p className='text-center text-gray-400 text-sm mb-6'>View and manage your tasks</p>
                     <ul className='space-y-4 overflow-auto max-h-96 scheme-dark bg-transparent px-5 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-800'>
-                        
-                        
-                        {
-                        
+                        {   
+                            doList.length === 0 ?
+                                <li className=' p-4 rounded-lg shadow text-center'>No tasks available</li>
+                            :
                             doList && doList.map((task: any, index: number) => (
                                 <li key={index} className='bg-gray-700 p-4 px-5 rounded-lg shadow'>
-                                    <h3 className='text-lg font-bold'>{task.title}</h3>
+                                    <div className='flex justify-between items-center '>
+                                        <h3 className='text-lg font-bold'>{task.title}</h3>
+                                        <button value={"delete"} onClick={() => deleteTask(task._id, index)} className='bg-red-500 hover:bg-red-600 text-white py-1 px-3 rounded mt-2'>Delete</button>
+                                    </div>
                                     <p className='text-gray-400 text-sm'>{task.description}</p>
                                     <div className='flex justify-between items-center mt-2'>
                                         <span className='text-gray-400 text-sm'>Due at: {new Date(task.date).toLocaleString()}</span>
                                         <div className='flex items-center'>
                                             <label className='mr-2'>Completed</label>
-                                            <input type="checkbox" className="accent-blue-500" />
+                                            <input type="checkbox" checked={task.completed} onChange={(e) => completedTask(index ,task._id, e.target.checked)} className="accent-blue-500" 
+                                            />
                                         </div>
                                     </div>
                                 </li>
